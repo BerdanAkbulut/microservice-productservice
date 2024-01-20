1632331bb7f4b963b301d3d45afa8b8c9d742a92
Berdan Akbulut <berdanakbulutt@hotmail.com>
2024-01-20 20:25:59 +0300

    deleted tst

--- src/test/java/com/BerdanAkbulut/saleproductservice/SaleProductserviceApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.BerdanAkbulut.saleproductservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SaleProductserviceApplicationTests {
-}
